b9a73122cef28b8d986e5a016bae06040cb26719
dahlys <dahlyx@gmail.com>
2017-10-18 12:33:16 -0700

    Add files via upload

---
+++ carShown.js
@@ -0,0 +1,10 @@
+var _Game_Event_setupPageSettings = Game_Event.prototype.setupPageSettings;
+Game_Event.prototype.setupPageSettings = function() {	
+	_Game_Event_setupPageSettings.call(this);
+	this.carShown = 0;
+};
+
+Game_Event.prototype.carShown = function() {
+	if (this._carShown === undefined) this.setupPageSettings.call(this);
+	return this._carShown;
+};
