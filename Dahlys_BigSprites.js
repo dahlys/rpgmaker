b953dec5a34b1a5a1be33a8619126347f5489a3b
dahlys <dahlyx@gmail.com>
2017-10-25 21:12:18 -0700

    Update Dahlys_BigSprites.js

--- Dahlys_BigSprites.js
+++ Dahlys_BigSprites.js
@@ -43,6 +43,10 @@
  * @desc Because getting off the side of the vehicle is left hand drive by default.
  * @default false
  * 
+ * @param Vehicle Reverse
+ * @desc Large Vehicle will reverse instead of flipping direction.
+ * @default true
+ * 
  * @param Touch Fix B
  * @desc Fix touch input if using type B big player/vehicle. Will overwrite findDirectionTo!
  * @default false
@@ -239,6 +243,8 @@
  * See Player section for what dx and dy mean.
  *
 */
+var Imported = Imported || {};
+Imported.BigSprites = true;
 
 var Dahlys = Dahlys || {};
 
@@ -265,6 +271,7 @@ var Dahlys = Dahlys || {};
 	Dahlys.sideLoad = eval(parameters['Enter Vehicle Side']);
 	Dahlys.sideUnload = eval(parameters['Exit Vehicle Side']);
 	Dahlys.RHD = eval(parameters['Right Hand Drive']);
+	Dahlys.vehicleReverse = eval(parameters['Vehicle Reverse']);
 	Dahlys.touchFixB = eval(parameters['Touch Fix B']);
 	
 /* 
@@ -321,14 +328,14 @@ var Dahlys = Dahlys || {};
 */		
 	
 	Game_Event.prototype.setSpriteSizeFromEventNote = function() {
-		if (this.isSpawnEvent) return;
+		if (Imported.Galv_EventSpawner && this.isSpawnEvent) return;
 		var note = $dataMap.events[this._eventId].meta.bigEvent;
 		if (!note) return;		
 		this.bigSpriteRegexProcessing(note);
 	};
 	
 	Game_Event.prototype.setSpriteSpSizeFromEventNote = function() {
-		if (this.isSpawnEvent) return;
+		if (Imported.Galv_EventSpawner && this.isSpawnEvent) return;
 		var note = $dataMap.events[this._eventId].meta.bigEventSp;
 		if (!note) return;
 		this.bigSpriteSpRegexProcessing(note);
@@ -802,6 +809,7 @@ var Dahlys = Dahlys || {};
 					if ($gamePlayer.x === x && $gamePlayer.y === y) {
 						$gamePlayer._vehicleType = vehicleType;
 						clearInterval(move);
+						if (Imported.SimpleLandVehicle && vehicleType === 'boat') $gamePlayer.playVehicleDoorSE();
 						return $gamePlayer._vehicleGettingOn;						
 					}
 				}, 300);
@@ -823,149 +831,126 @@ var Dahlys = Dahlys || {};
 	
 	Dahlys.getOffCoord = null;
 	
-	var _Game_Vehicle_isLandOk = Game_Vehicle.prototype.isLandOk;
-	Game_Vehicle.prototype.isLandOk = function(x, y, d) {
-		if ($gamePlayer._bigSprite.type) {
-			if (Imported.YEP_X_VehicleRestrict) {
-				var regionCanLand = this.checkLandingRegionId();
+	var _Game_Player_getOffVehicle = Game_Player.prototype.getOffVehicle;
+	Game_Player.prototype.getOffVehicle = function() {
+		if (this._bigSprite.type) {
+			if (Imported.YEP_X_VehicleRestrict) {if (!Dahlys.checkLandingRegionId()) return;} 
+			if (this.isInAirship()) {
+				if (!Dahlys.isBigAirshipLandOk) return;
 			} else {
-				var regionCanLand = true;
+				var d = this._direction;
+				if (Dahlys.sideUnload) {
+					if (Dahlys.RHD) {
+						if (d === 2) {var d1 = 6; var d2 = 4;}
+						if (d === 8) {var d1 = 4; var d2 = 6;}
+						if (d === 4) {var d1 = 2; var d2 = 8;}
+						if (d === 6) {var d1 = 8; var d2 = 2;}
+					} else {
+						if (d === 2) {var d1 = 4; var d2 = 6;}
+						if (d === 8) {var d1 = 6; var d2 = 4;}
+						if (d === 4) {var d1 = 8; var d2 = 2;}
+						if (d === 6) {var d1 = 2; var d2 = 8;}
+					}
+					if (!Dahlys.checkVehicleExit(d1)) {
+						Dahlys.checkSideExit(d2);
+						if (!Dahlys.checkVehicleExit(d2)) return;
+					}						
+				} else if (!Dahlys.checkVehicleExit(d)) return;				
 			}
-			if (regionCanLand) {
-				if (this.isAirship()) {
-					if (!$gameMap.isAirshipLandOk(x, y)) {
-						return false;
-					}
-					if ($gameMap.eventsXy(x, y).length > 0) {
-						return false;
-					}
-				} else {				
-					if (Dahlys.sideUnload) {
-						if (Dahlys.RHD) {
-							if (d === 2) {var d1 = 6; var d2 = 4;}
-							if (d === 8) {var d1 = 4; var d2 = 6;}
-							if (d === 4) {var d1 = 2; var d2 = 8;}
-							if (d === 6) {var d1 = 8; var d2 = 2;}
-						} else {
-							if (d === 2) {var d1 = 4; var d2 = 6;}
-							if (d === 8) {var d1 = 6; var d2 = 4;}
-							if (d === 4) {var d1 = 8; var d2 = 2;}
-							if (d === 6) {var d1 = 2; var d2 = 8;}
-						}
-						var side1Tiles = this.checkAheadTiles.call($gamePlayer, d1);
-						var options = [];
-						for (var i = 0; i < side1Tiles.length; i++) {
-							if ($gameMap.isValid(side1Tiles[i].x, side1Tiles[i].y) && $gameMap.isPassable(side1Tiles[i].x, side1Tiles[i].y, this.reverseDir(d1)) && !this.isCollidedWithCharacters(side1Tiles[i].x, side1Tiles[i].y)) {
-								var plusX = 0;
-								var plusY = 0;
-								if (d1 === 2) plusY = -1;
-								if (d1 === 6) plusX = -1;
-								if (d1 === 8) plusY = 1;
-								if (d1 === 4) plusX = 1;
-								options.push({'x': side1Tiles[i].x + plusX, 'y': side1Tiles[i].y + plusY, 'd': d1});
-							}
-						}
-						if (options.length > 0) {
-							for (var i = 0; i < options.length; i++) {
-								if ($gameMap.isLoopHorizontal()) {
-									if (options[i].x >= $gameMap.width()) {options[i].x -= $gameMap.width();}
-									else if (options[i].x < 0) {options[i].x += $gameMap.width();};
-								}
-								if ($gameMap.isLoopVertical()) {
-									if (options[i].y >= $gameMap.height()) {options[i].y -= $gameMap.height();}
-									else if (options[i].y < 0) {options[i].y += $gameMap.height();};
-								}
-							}
-							if (options.length % 2 === 0) {
-								if (d === 8) var middleOption = options.length/2 - 1;
-								else var middleOption = options.length/2;
-							} else {
-								var middleOption = Math.floor(options.length/2);
-							}
-							Dahlys.getOffCoord = options[middleOption];
-							return true;
-						}
-						var side2Tiles = this.checkAheadTiles.call($gamePlayer, d2);
-						for (var i = 0; i < side2Tiles.length; i++) {
-							if ($gameMap.isValid(side2Tiles[i].x, side2Tiles[i].y) && $gameMap.isPassable(side2Tiles[i].x, side2Tiles[i].y, this.reverseDir(d2)) && !this.isCollidedWithCharacters(side2Tiles[i].x, side2Tiles[i].y)) {
-								var plusX = 0;
-								var plusY = 0;
-								if (d2 === 2) plusY = -1;
-								if (d2 === 6) plusX = -1;
-								if (d2 === 8) plusY = 1;
-								if (d2 === 4) plusX = 1;
-								options.push({'x': side2Tiles[i].x + plusX, 'y': side2Tiles[i].y + plusY, 'd': d2});
-							}
-						}
-						if (options.length > 0) {
-							for (var i = 0; i < options.length; i++) {
-								if ($gameMap.isLoopHorizontal()) {
-									if (options[i].x >= $gameMap.width()) {options[i].x -= $gameMap.width();}
-									else if (options[i].x < 0) {options[i].x += $gameMap.width();};
-								}
-								if ($gameMap.isLoopVertical()) {
-									if (options[i].y >= $gameMap.height()) {options[i].y -= $gameMap.height();}
-									else if (options[i].y < 0) {options[i].y += $gameMap.height();};
-								}
-							}
-							if (options.length % 2 === 0) {
-								if (d === 8) var middleOption = options.length/2 - 1;
-								else var middleOption = options.length/2;
-							} else {
-								var middleOption = Math.floor(options.length/2);
-							}
-							Dahlys.getOffCoord = options[middleOption];
-							return true;
-						}
-						return false;
-					}
-					var forwardTiles = this.checkAheadTiles.call($gamePlayer, d);
-					for (var i = 0; i < forwardTiles.length; i++) {
-						if ($gameMap.isValid(forwardTiles[i].x, forwardTiles[i].y) && $gameMap.isPassable(forwardTiles[i].x, forwardTiles[i].y, this.reverseDir(d)) && !this.isCollidedWithCharacters(forwardTiles[i].x, forwardTiles[i].y)) {
-							var plusX = 0;
-							var plusY = 0;
-							if (this._direction === 2) plusY = -1;
-							if (this._direction === 6) plusX = -1;
-							if (this._direction === 8) plusY = 1;
-							if (this._direction === 4) plusX = 1;
-							Dahlys.getOffCoord = {'x': forwardTiles[i].x + plusX, 'y': forwardTiles[i].y + plusY, 'd': this._direction};
-							if ($gameMap.isLoopHorizontal()) {
-								if (Dahlys.getOffCoord.x >= $gameMap.width()) {Dahlys.getOffCoord.x -= $gameMap.width();}
-								else if (Dahlys.getOffCoord.x < 0) {Dahlys.getOffCoord.x += $gameMap.width();};
-							}
-							if ($gameMap.isLoopVertical()) {
-								if (Dahlys.getOffCoord.y >= $gameMap.height()) {Dahlys.getOffCoord.y -= $gameMap.height();}
-								else if (Dahlys.getOffCoord.y < 0) {Dahlys.getOffCoord.y += $gameMap.height();};
-							}
-							return true;
-						}
-					}
-					return false;
-				}
-			} else {
-				return false;
-			}				
-		}
-		return _Game_Vehicle_isLandOk.call(this, x, y, d);		
-	};
+			var vehicle = this.vehicle();
+			this._followers.synchronize(this.x, this.y, this.direction());
+			$gamePlayer.initializeBigSprite();		
+			this.vehicle().getOff();
+			if (Dahlys.getOffCoord) {
+				var x = Dahlys.getOffCoord.x;
+				var y = Dahlys.getOffCoord.y;
+				while ($gamePlayer.x < x) $gamePlayer.forceMoveDirection(6);
+				while ($gamePlayer.x > x) $gamePlayer.forceMoveDirection(4);
+				while ($gamePlayer.y < y) $gamePlayer.forceMoveDirection(2);
+				while ($gamePlayer.y > y) $gamePlayer.forceMoveDirection(8);
+				$gamePlayer.setDirection(Dahlys.getOffCoord.d);
+			}
+			Dahlys.getOffCoord = null;
+			$gamePlayer._bigSprite = $gameSystem._bigSprite;
+			$gameSystem.setupBigPlayerSettings();
+			vehicle.setBigVehicleSize(vehicle._type);
+			vehicle.setBigSpriteCoordinates();
+			$gamePlayer.setBigSpriteCoordinates();
+			if (Imported.SimpleLandVehicle) this.playVehicleDoorSE();
+			if (!this.isInAirship()) {
+				this.forceMoveForward();
+				this.setTransparent(false);
+			}
+			this._vehicleGettingOff = true;
+			this.setMoveSpeed(4);
+			this.setThrough(false);
+			this.makeEncounterCount();
+			this.gatherFollowers();
+			return this._vehicleGettingOff;
+		} else return _Game_Player_getOffVehicle.call(this);
+	}
 	
-	var _Game_Map_isAirshipLandOk = Game_Map.prototype.isAirshipLandOk;
-	Game_Map.prototype.isAirshipLandOk = function(x, y) {
+	Dahlys.checkVehicleExit = function(d) {
+		var sideTiles = $gamePlayer.checkAheadTiles(d);
+		var options = [];
+		for (var i = 0; i < sideTiles.length; i++) {
+			var tempX = $gameMap.roundXWithDirection(sideTiles[i].x, 10 - d);
+			var tempY = $gameMap.roundYWithDirection(sideTiles[i].y, 10 - d);
+			if ($gamePlayer.vehicle().isLandOk(tempX, tempY, d)) {
+				var plusX = 0;
+				var plusY = 0;
+				if (d === 2) plusY = -1;
+				if (d === 6) plusX = -1;
+				if (d === 8) plusY = 1;
+				if (d === 4) plusX = 1;
+				options.push({'x': sideTiles[i].x + plusX, 'y': sideTiles[i].y + plusY, 'd': d});
+			}
+		}
+		if (options.length > 0) {
+			for (var i = 0; i < options.length; i++) {
+				if ($gameMap.isLoopHorizontal()) {
+					if (options[i].x >= $gameMap.width()) {options[i].x -= $gameMap.width();}
+					else if (options[i].x < 0) {options[i].x += $gameMap.width();};
+				}
+				if ($gameMap.isLoopVertical()) {
+					if (options[i].y >= $gameMap.height()) {options[i].y -= $gameMap.height();}
+					else if (options[i].y < 0) {options[i].y += $gameMap.height();};
+				}
+			}
+			if (options.length % 2 === 0) {
+				if (d === 8) var middleOption = options.length/2 - 1;
+				else var middleOption = options.length/2;
+			} else {
+				var middleOption = Math.floor(options.length/2);
+			}
+			Dahlys.getOffCoord = options[middleOption];
+			return true;
+		}
+		return false;
+	};	
+	
+	Dahlys.isBigAirshipLandOk = function() {
 		if ($gamePlayer._bigSprite.type) {
 			var coord = $gamePlayer._bigSprite.occupancy;
 			var check = true;
 			for (var i = 0; i < coord.length; i++) {
-				check = check && this.checkPassage(coord[i].x, coord[i].y, 0x0800) && this.checkPassage(coord[i].x, coord[i].y, 0x0f);
+				check = check && $gameMap.checkPassage(coord[i].x, coord[i].y, 0x0800) && $gameMap.checkPassage(coord[i].x, coord[i].y, 0x0f) && $gameMap.eventsXy(coord[i].x, coord[i].y).length > 0;
 				if (!check) return check;
 			}
 			return check;
 		}
-		return _Game_Map_isAirshipLandOk.call(this, x, y);
 	};
-	
+
+	var _Game_Vehicle_getOff = Game_Vehicle.prototype.getOff;
+	Game_Vehicle.prototype.getOff = function() {
+		var dir = this._direction;
+		_Game_Vehicle_getOff.call(this);
+		this.setDirection(dir);	
+	};
+		
 	if (Imported.YEP_X_VehicleRestrict) {
-		Game_Vehicle.prototype.checkLandingRegionId = function() {
-			var vehicle = this._type;
+		Dahlys.checkLandingRegionId = function() {
+			var vehicle = $gamePlayer.vehicle()._type;
 			var coord = $gamePlayer._bigSprite.occupancy;
 			var regionId = [];
 			for (var i = 0; i < coord.length; i++) {
@@ -979,29 +964,17 @@ var Dahlys = Dahlys || {};
 			return true;
 		};
 	}
-
-	var _Game_Vehicle_getOff = Game_Vehicle.prototype.getOff;
-	Game_Vehicle.prototype.getOff = function() {
-		var dir = this._direction;
-		$gamePlayer.initializeBigSprite();		
-		_Game_Vehicle_getOff.call(this);
-		this.setDirection(dir);
-		if (Dahlys.getOffCoord) {
-			var x = Dahlys.getOffCoord.x;
-			var y = Dahlys.getOffCoord.y;
-			while ($gamePlayer.x < x) $gamePlayer.forceMoveDirection(6);
-			while ($gamePlayer.x > x) $gamePlayer.forceMoveDirection(4);
-			while ($gamePlayer.y < y) $gamePlayer.forceMoveDirection(2);
-			while ($gamePlayer.y > y) $gamePlayer.forceMoveDirection(8);
-			$gamePlayer.setDirection(Dahlys.getOffCoord.d);
-		}
-		Dahlys.getOffCoord = null;
-		$gamePlayer._bigSprite = $gameSystem._bigSprite;
-		$gameSystem.setupBigPlayerSettings();
-		this.setBigVehicleSize(this._type);
-		this.setBigSpriteCoordinates();
-		$gamePlayer.setBigSpriteCoordinates();
-	};
+	
+	if (Imported.SimpleLandVehicle && Dahlys.sideUnload) {
+		var _Game_Player_checkNoLandVehicleRegionID = Game_Player.prototype.checkNoLandVehicleRegionID;
+		Game_Player.prototype.checkNoLandVehicleRegionID = function(x, y, d) {
+			if ($gamePlayer.isInVehicle() && $gamePlayer._bigSprite.type) {
+				return $gameMap.regionId(x, y);
+			} else {
+				return _Game_Player_checkNoLandVehicleRegionID.call(this, x, y, d);
+			}
+		};
+	}
 	
 /* 
 -------------------------------------------------------------------------------------------------------------------------------------------
@@ -1210,6 +1183,7 @@ var Dahlys = Dahlys || {};
 	var _Game_CharacterBase_setDirection = Game_CharacterBase.prototype.setDirection;
 	Game_CharacterBase.prototype.setDirection = function(d) {
 		if (!$gameMap || !$gameMap.tileEvents) return _Game_CharacterBase_setDirection.call(this, d);
+		if (Dahlys.vehicleReverse && this === $gamePlayer && this.isInVehicle() && this._direction === 10 - d) return;
 		if (this._bigSprite.type) {
 			oldCoord = this._bigSprite.occupancy; 
 			oldDir = this._direction;
@@ -1237,8 +1211,14 @@ var Dahlys = Dahlys || {};
 	var _Game_Character_moveStraight = Game_Character.prototype.moveStraight;
 	Game_Character.prototype.moveStraight = function(d) {		
 		if (this._bigSprite.type) {
-			this.setDirection(d);
-			if (d === this._direction) { 
+			var success = false;
+			if (Dahlys.vehicleReverse && this === $gamePlayer && this.isInVehicle() && this._direction === 10 - d) {
+				success = true;
+			} else {
+				this.setDirection(d);
+				if (d === this._direction) success = true;
+			}
+			if (success) { 
 				this.setMovementSuccess(this.canPass(this._x, this._y, d));			
 				if (this.isMovementSucceeded()) {
 					this._x = $gameMap.roundXWithDirection(this._x, d);
